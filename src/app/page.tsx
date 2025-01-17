551c2fadc4fd53d67bbe615e7d68fb31bdcb3cc8
John Michael Goco <jmgoco416@gmail.com>
2025-01-17 20:49:36 +0800

    Force Dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import {db} from "~/server/db"
 
+export const dynamic = "force-dynamic"
+
 const mockUrls = [
   "https://ziscc8xnvc.ufs.sh/f/uPEu0Jc8nQrbx43TULB8DBpqLkZjAPYgXCeKRvm6ldiSIFcs",
   "https://ziscc8xnvc.ufs.sh/f/uPEu0Jc8nQrbXl09F3i1dqfhiG9WKbIYuHtolMZ65cwCOBF0",
@@ -25,7 +27,7 @@ export default async function HomePage() {
         {mockImages.map((image) => (
           <div key={image.id} className="w-48 p-1">
             <img
-              src={image.url} className="w-full h-full object-cover rounded-md"
+              src={image.url} className="w-ful l h-full object-cover rounded-md"
             />
           </div>
         ))}
