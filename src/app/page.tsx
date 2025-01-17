af655de724280634c39f030a537edf417e3477ea
John Michael Goco <jmgoco416@gmail.com>
2025-01-17 20:35:35 +0800

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import {db} from "~/server/db"
 
 const mockUrls = [
   "https://ziscc8xnvc.ufs.sh/f/uPEu0Jc8nQrbx43TULB8DBpqLkZjAPYgXCeKRvm6ldiSIFcs",
@@ -11,10 +12,16 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48 p-1">
             <img
