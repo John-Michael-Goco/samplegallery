338593d4c7cbcfc6d762e4fdd3a8f5243ba89cd1
John Michael Goco <jmgoco416@gmail.com>
2025-02-01 16:57:47 +0800

    Did a Testing

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,17 +1,29 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+"use client";
+
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
-    return(
-    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-        <div>Sample Gallery</div>
+    const router = useRouter();
 
-        <div>
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
-        </div>
-    </nav>)
+    return (
+        <nav className="text-x1 flex items-center justify-between border-b p-4 font-semibold">
+            <div>Gallery</div>
+            <div>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UploadButton
+                        endpoint="imageUploader"
+                        onClientUploadComplete={() => {
+                            router.refresh();
+                        }}
+                    />
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </nav>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,43 @@
-import { SignedIn, SignedOut } from "@clerk/nextjs";
-import Link from "next/link";
-import {db} from "~/server/db"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";      
+import { eq } from "drizzle-orm";
+import { UploadThingError } from "uploadthing/server";
+import { db } from "~/server/db";
+
 
 export const dynamic = "force-dynamic"
 
-async function Images(){
-  const images = await db.query.images.findMany({
-    orderBy: (module, {desc}) => desc(module.id)
-  });
+async function Images() {
+  const user = await auth();
+  // If you throw, the user will not be able to upload
+    if (!user.userId) throw new UploadThingError("Unauthorized");
 
-  return(  
-  <div className="flex flex-wrap gap-4">
-    {images.map((image) => (
-      <div key={image.id} className="w-48 p-1">
-        <img
-          src={image.url} className="w-ful l h-full object-cover rounded-md"
-        />
-        <div> {image.name}</div>
-      </div>
-    ))}
-  </div>
+  const images = await db.query.images.findMany({
+    where: (model) => eq(model.userId, user.userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} alt={image.name} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
   );
 }
 
 export default async function HomePage() {
+
+  
   return (
-    <main className="">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <SignedOut>
-        <div className="h-full w-full text-2xl text-center">
-          Please Sign In Above
-          </div>
+        <div className="h-full w-full text-2x1">Please Sign In Above</div>
       </SignedOut>
       <SignedIn>
-        <Images />
+        <Images/>
       </SignedIn>
     </main>
   );
